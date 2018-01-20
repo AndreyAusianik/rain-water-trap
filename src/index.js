1b4644dd76a284f95c89cc99a85d5dff50d2e61c
Andrei Ausianik <andrei.ausianik@iii.com>
2018-01-20 14:56:20 +0300

    rain-water-trap

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function trapRainWater(elevationMap) {
-    // your solution here
+    let max = 0;
+    let mountain = [0];
+
+    for(let i = 0; i< elevationMap.length; i++) {
+        const element = elevationMap[i];
+        if(element > max) {
+            while(mountain[mountain.length - 1] < max) mountain.pop();
+            mountain.push(element);
+            max = element;
+        } else {
+            while(mountain[mountain.length - 1] < element) mountain.pop();
+            mountain.push(element);
+        }
+    }
+    let mountainIndex = 1;
+    let waterVol = 0;
+    elevationMap.forEach(element => {
+        waterVol += Math.max(0, Math.min(mountain[mountainIndex], mountain[mountainIndex - 1]) - element);
+        if(element == mountain[mountainIndex]) {
+            mountainIndex++;
+        }
+    });
+    return waterVol;
 }
